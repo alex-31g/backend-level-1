42ea212fbb92b540a064c34f76280d40af0f021d
alex-31g <al.tsimbalyuk@gmail.com>
2021-05-12 04:57:15 +0300

    add

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 import express from 'express';
 
+// ---------------------------
+// Чтобы использовать __dirname в ES6 нужно выполнить следующее:
+import path from 'path';
+const __dirname = path.resolve();
+// ---------------------------
+
 const PORT = 5000;
 const app = express();
 
-// Парсим данные в json
-app.use(express.json()); // <---
+// Парсим данные формы
+app.use(express.urlencoded()); 
 
-app.post('/', (req, res) => {
-	let body = req.body; // <---
-	res.json(body);
+app.get('/register', (req, res) => {
+	res.sendFile(__dirname + '/register.html');
+});
+
+app.post('/register', (req, res) => {
+	if (!req.body) {
+		return res.sendStatus(400);
+	}
+	console.log(req.body);
+	res.send(`${req.body.userName} - ${req.body.userAge}`);
 });
 
 app.listen(PORT, () => { console.log(`Server started at ${PORT} port`)});
